14f3f474617d7f8b48f545894af729da3905c795
hieuhp01 <46868429+hieuhp01@users.noreply.github.com>
2023-01-19 21:49:45 +0100

    Update game_management.cpp

--- Project2/game_management.cpp
+++ Project2/game_management.cpp
@@ -12,22 +12,28 @@ game_management::~game_management()
 }
 void game_management::init(string title) 
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) 
+    {
         isRunning = false;
     }
-    else {
+    else 
+    {
         gWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (gWindow == NULL) {
+        if (gWindow == NULL) 
+        {
             isRunning = false;
         }
-        else {
+        else 
+        {
             SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 
             gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-            if (gRenderer == NULL) {
+            if (gRenderer == NULL)
+            {
                 isRunning = false;
             }
-            else {
+            else
+            {
                 SDL_SetRenderDrawColor(gRenderer, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
 
                 int imgFlags = IMG_INIT_PNG;
@@ -57,12 +63,15 @@ void game_management::init(string title)
     //init chicken
     int t = 0;
     int y_row = 0;
-    for (int c = 0; c < NUMBER_OF_CHICKEN; c++) {
+    for (int c = 0; c < NUMBER_OF_CHICKEN; c++) 
+    {
         Chicken* p_chicken = new Chicken();
-        if (p_chicken) {
+        if (p_chicken) 
+        {
             p_chicken->loadImg("PNG//chicken_red.png", gRenderer);
             p_chicken->set_clips();
-            if (t % NUMBER_OF_CHICKENS_PER_ROW == 0) {
+            if (t % NUMBER_OF_CHICKENS_PER_ROW == 0) 
+            {
                 y_row -= DISTANCE_BETWEEN_CHICKENS;
                 t = 0;
             }
@@ -71,7 +80,8 @@ void game_management::init(string title)
             p_chicken->set_x_val(CHICKEN_SPEED);
             p_chicken->set_heart(CHICKEN_HEART);
             int random = rand() % 10;
-            if (random < 3) {
+            if (random < 3) 
+            {
                 bullet* p_bullet = new bullet();
                 p_chicken->InitBullet(p_bullet, gRenderer);
             }
@@ -96,7 +106,8 @@ void game_management::init(string title)
     isRunning = true;
 }
 
-bool game_management::check_collision(const SDL_Rect& object1, const SDL_Rect& object2) {
+bool game_management::check_collision(const SDL_Rect& object1, const SDL_Rect& object2)
+{
     int left_a = object1.x;
     int right_a = object1.x + object1.w;
     int top_a = object1.y;
@@ -107,7 +118,8 @@ bool game_management::check_collision(const SDL_Rect& object1, const SDL_Rect& o
     int top_b = object2.y;
     int bottom_b = object2.y + object2.h;
 
-    if (left_a > right_b || right_a < left_b || top_a > bottom_b || bottom_a < top_b) {
+    if (left_a > right_b || right_a < left_b || top_a > bottom_b || bottom_a < top_b) 
+    {
         return false;
     }
     return true;
@@ -125,11 +137,15 @@ void game_management::handle_event()
     }
 }
 
-void game_management::handle_chicken() {
-    if (kill < NUMBER_OF_CHICKEN * 5) {
-        for (int ck = 0; ck < p_chicken_list.size(); ck++) {
+void game_management::handle_chicken()
+{
+    if (kill < NUMBER_OF_CHICKEN * 5) 
+    {
+        for (int ck = 0; ck < p_chicken_list.size(); ck++) 
+        {
             Chicken* p_chicken = p_chicken_list.at(ck);
-            if (p_chicken) {
+            if (p_chicken)
+            {
                 p_chicken->Move();
                 p_chicken->Show(gRenderer);
                 p_chicken->HandleBullet(gRenderer);
@@ -138,11 +154,14 @@ void game_management::handle_chicken() {
             //check spaceship with chicken_bullet
             bool Col1 = false;
             vector<bullet*> bullet_list = p_chicken->get_bullet_list();
-            for (int b = 0; b < bullet_list.size(); b++) {
+            for (int b = 0; b < bullet_list.size(); b++)
+            {
                 bullet* p_bullet = bullet_list.at(b);
-                if (p_bullet) {
+                if (p_bullet)
+                {
                     Col1 = check_collision(p_bullet->GetRect(), spaceship.GetRect());
-                    if (Col1 == true) {
+                    if (Col1 == true)
+                    {
                         p_chicken->RemoveBullet(b);
                         break;
                     }
@@ -151,7 +170,8 @@ void game_management::handle_chicken() {
 
             //check spacecraft with chicken
             bool Col2 = check_collision(spaceship.GetRect(), p_chicken->GetRectFrame());
-            if (Col1 || Col2) {
+            if (Col1 || Col2)
+            {
                 
                 //set exp
                 int x_pos = (spaceship.GetRect().x + WIDTH_MAIN / 2) - WIDTH_FRAME_EXP / 2;
@@ -167,20 +187,25 @@ void game_management::handle_chicken() {
 
             //check spaceship_bullet with chicken
             vector<bullet*> s_bullet_list = spaceship.get_bullet_list();
-            for (int sb = 0; sb < s_bullet_list.size(); sb++) {
+            for (int sb = 0; sb < s_bullet_list.size(); sb++) 
+            {
                 bullet* p_bullet = s_bullet_list.at(sb);
-                if (p_bullet != NULL) {
+                if (p_bullet != NULL) 
+                {
                     bool Col3 = check_collision(p_bullet->GetRect(), p_chicken->GetRectFrame());
-                    if (Col3) {
+                    if (Col3) 
+                    {
                         p_chicken->Decrease((spaceship.get_bullet_damage()));
                         spaceship.RemoveBullet(sb);
 
-                        if (p_chicken->get_heart() <= 0) {
+                        if (p_chicken->get_heart() <= 0)
+                        {
                             p_chicken->set_heart(CHICKEN_HEART);
                             kill++;
 
                             p_chicken->SetRect(p_chicken->GetRect().x, -3 * SCREEN_HEIGHT);
-                            if (kill > NUMBER_OF_CHICKEN*5) {
+                            if (kill > NUMBER_OF_CHICKEN*5)
+                            {
                                 p_chicken->set_come_back(false);
                             }
                         }
@@ -230,8 +255,10 @@ void game_management::handle_game()
             end_game.SetRect(310, SCREEN_HEIGHT / 5);
             end_game.loadText_showText(g_font_end_game, gRenderer);
     }
-    else {
-        if (spaceship.get_status() == false) {
+    else 
+    {
+        if (spaceship.get_status() == false) 
+        {
             hint.SetText("Press 'ENTER' to revive !");
             hint.SetRect(280, SCREEN_HEIGHT / 4);
             hint.loadText_showText(g_font_menu, gRenderer);
