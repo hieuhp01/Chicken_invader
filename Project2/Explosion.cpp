fa0ad9f2879fded16bb66b3b29d8e3a85b2b79e7
hieuhp01 <46868429+hieuhp01@users.noreply.github.com>
2023-01-19 21:46:34 +0100

    Update Explosion.cpp

--- Project2/Explosion.cpp
+++ Project2/Explosion.cpp
@@ -1,13 +1,17 @@
 #include "Explosion.h"
 
-Explosion::Explosion() {
+Explosion::Explosion() 
+{
     frame_ = 16;
 }
-Explosion::~Explosion() {
+Explosion::~Explosion() 
+{
 
 }
-void Explosion::set_clip() {
-    for (int i = 0; i < 8; i++) {
+void Explosion::set_clip() 
+{
+    for (int i = 0; i < 8; i++) 
+    {
         clip_[i].x = i * WIDTH_FRAME_EXP;
         clip_[i].y = 0;
         clip_[i].w = WIDTH_FRAME_EXP;
@@ -15,7 +19,8 @@ void Explosion::set_clip() {
     }
 }
 
-void Explosion::Show(SDL_Renderer* screen) {
+void Explosion::Show(SDL_Renderer* screen) 
+{
     frame_++;
     SDL_Rect renderQuad = { rect_.x ,rect_.y , WIDTH_FRAME_EXP ,HEIGHT_FRAME_EXP };
     SDL_RenderCopy(screen, object_, &clip_[frame_ / 2], &renderQuad);
